51ff9f189556d4b4b32abb14a32532117d8f5a77
BhavyaBibra <41470348+BhavyaBibra@users.noreply.github.com>
2020-08-07 16:42:55 +0530

    Add files via upload

---
+++ Trying the if else statements.py
@@ -0,0 +1,9 @@
+x = input("Y/N")
+x = str(x)
+if x == 'Y' or "y":
+    print("Great YES!")
+if x == 'N' or 'n':
+    print("NOOOO")
+else:
+    print("This shouldn't be shown.")
+var = input("ENter key.")
